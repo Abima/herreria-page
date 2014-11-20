b4752561fab333f01e8e89d0b882470355a94150
Abima <cervantes0088@hotmail.com>
2014-11-20 14:58:20 -0600

    codigo.js

--- js/codigo.js
+++ js/codigo.js
@@ -5,22 +5,23 @@ $(document).on('ready',function(){
 		$('#menu').css('box-shadow','none');
 
 	});
-		
-});
-
-$(document).ready(function () {       
 	if ($('html').hasClass('desktop')) {
   		new WOW().init();
 	}
-	var _gaq = _gaq || [];
-	 _gaq.push(['_setAccount', 'UA-7078796-5']);
-	 _gaq.push(['_trackPageview']);
-	 (function() {
-	   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	 })();   
+	wow = new WOW(
+	  {
+	    animateClass: 'animated',
+	    offset:       100
+	  }
+	);
+	wow.init();
+	document.getElementById('moar').onclick = function() {
+	  var section = document.createElement('section');
+	  section.className = 'section--purple wow fadeInDown';
+	  this.parentNode.insertBefore(section, this);
+	};
 });
+
 
 
 
